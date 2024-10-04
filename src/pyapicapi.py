399fd3b804a9c188bc347049caf68a6706186bcd
kkyick2 <kkyick2@gmail.com>
2024-10-04 16:21:52 +0800

    20241004 1621

--- src/pyapicapi.py
+++ src/pyapicapi.py
@@ -9,8 +9,10 @@ LOG_ENV = 'dev'
 LOG_DIR = 'log'
 CONFIG_DIR = 'config'
 
-def setup_logging():
-    """Load logging configuration"""
+def get_datetime():
+    return datetime.now().strftime("%Y%m%d_%H%M")
+
+def setup_logging() -> None:
     log_configs = {
         "dev": "logging.dev.json", 
         "prod": "logging.prod.json"
@@ -30,8 +32,7 @@ def setup_logging():
     logging.config.dictConfig(config)
     return
 
-def get_apic_token(ip, username, password):
-    logger.info(f'### Step4 - Login apic get token:')
+def get_apic_token(ip: str, username: str, password: str) -> str:
     url = f'https://{ip}/api/aaaLogin.json'
     payload = {
         "aaaUser": {
@@ -45,6 +46,8 @@ def get_apic_token(ip, username, password):
         "Accept":"application/json",
         "Content-Type": "application/json"
     }
+
+    logger.info(f'###### Step4 - Login apic and get token:')
     requests.packages.urllib3.disable_warnings()
     resp = requests.post(url, headers=headers, data=json.dumps(payload), verify=False)
     token = resp.json()['imdata'][0]['aaaLogin']['attributes']['token']
@@ -52,8 +55,7 @@ def get_apic_token(ip, username, password):
     logger.info(f'Token: {token}')
     return token
 
-
-def get_apic_api_resp(ip, key, token):
+def get_apic_api_resp(ip: str, key: str, token: str) -> requests.Response:
     url = f'https://{ip}/api/class/{key}.json'
     payload={}
     headers = {
@@ -62,39 +64,46 @@ def get_apic_api_resp(ip, key, token):
         "Cookie" : f'APIC-Cookie={token}'
     }
 
-    logger.info(f'### Step5 - Login apic restapi: {key} - {url}')
+    logger.info(f' Step5A - Login apic api: {key} - {url}')
     resp = requests.get(url, headers=headers, data=payload, verify=False)
     return resp
 
-def parse_apic_json_to_df(key, json_obj):
+def parse_apic_json_to_df(key: str, json_obj: list) -> pd.DataFrame:
     # parse apic json to dataframe
     #               lv1        lv2(key)       lv3
     # json_obj = {'imdata': [{'fabricPod': {'attributes': {'childAction ...  || <class 'dict'>
     # data_list = json_obj[imdata] = {'fabricPod': {'attributes': {'childAction ...}, <class 'list'>
     #                                {'fabricPod': {'attributes': {'childAction ...},
-    lv1 = 'imdata'
-    lv2 = key
-    lv3 = 'attributes'
     parsed_data = []
-    data_list = json_obj[lv1] 
-    for data in data_list:
-        parsed_data.append(data[lv2][lv3])
+    for data in json_obj['imdata']:
+        parsed_data.append(data[key]['attributes'])
     df = pd.DataFrame(parsed_data)
-    logger.info(f' Table size: {df.shape}')
+    logger.info(f' Step5B - Exported to dataframe, size: {df.shape}')
     return df
 
-def get_config_files_to_list(dir):
-    logger.info(f'### Step1 - Get config files in: {dir}')
+def remove_columns(df: pd.DataFrame, properties: list) -> pd.DataFrame:
+    for i in properties:
+        df.pop(i)
+    logger.info(f' Step5C - Removed properties size: {df.shape}')
+    return df
+
+def export_df_to_xlsx(writer: pd.ExcelWriter, df: pd.DataFrame, key: str) -> None:
+    df.to_excel(writer, sheet_name=key, index=False)
+    ws = writer.sheets[key]
+    return
+
+def get_config_files_to_list(dir: str) -> list:
+    logger.info(f'###### Step1 - Get config files in: {dir}')
     matched_files = []
     files = os.listdir(dir)
     for f in files:
-        m = re.search("(.*).txt",f)
+        m = re.search("(.*)input.json",f)
         if m:
             matched_files.append(f)
     return matched_files
 
-def promt_user_select_file(files):
-    logger.info(f'### Step2 - Choose input config files from list:')
+def promt_user_select_file(files: list) -> str:
+    logger.info(f'###### Step2 - Choose input config files from list:')
     if len(files) == 1:
         logger.info(f' 0) {files[0]}')
         selected_file = files[0]
@@ -108,66 +117,16 @@ def promt_user_select_file(files):
     logger.info(f' Selected file: {selected_file}')
     return selected_file 
 
-def get_datetime():
-    return datetime.now().strftime("%Y%m%d_%H%M")
-
-def export_df_to_xlsx(writer, df, key):
-    df.to_excel(writer, sheet_name=key, index=False)
-    ws = writer.sheets[key]
-    return
-
-def read_config_file(in_file):
-    logger.info(f'### Step3 - Loading details from {in_file}:')
-    description = []
-    table = []
-    sheetname = []
-    properties = []
+def read_config_json(in_file: str) -> list:
+    logger.info(f'###### Step3 - Load json config from {in_file}')
     with open(in_file,'r') as f:
-        for line in f:
-            line = line.rstrip("\n\r")
-            if (re.match("^#",line)):
-                continue
-            m = re.search("\$login=\"(.*)\"",line)
-            if m:
-                username = m.group(1)
-                continue
-            m = re.search("\$APIC_IP=\"(.*)\"",line)
-            if m:
-                ip = m.group(1)
-                continue
-            m = re.search("\$password=\"(.*)\"",line)
-            if m:
-                password = m.group(1)
-                continue
-            m = re.search("\$Environment=\"(.*)\"",line)
-            if m:
-                environment = m.group(1)
-                continue
-            m = re.search("\$remove_properties=(.*)",line)
-            if m:
-                remove_properties_flag = int(m.group(1))
-                continue
-            m = re.search("^\"",line)
-            if m:
-                list = line.split(",")
-                description.append(list[0].replace('"',""))
-                table.append(list[1].replace('"',""))
-                sheetname.append(list[2].replace('"',""))
-            elif re.search("^\'",line):
-                line = line.replace(" ","")
-                properties.append(line)
-    login_info = {
-        'ip': ip,
-        'username': username,
-        'password':  password,
-        'environment': environment,
-        'remove_properties' : remove_properties_flag
-    }
-    return [login_info, description, table, sheetname, properties]
+        config = json.load(f)
+    logger.info(f" Info collected, tables to process: {len(config['tables'])}")
+    return [config['login'], config['tables']]
 
-def process_script():
-
+def process_script() -> None:
     config_dir = os.path.join(PARENT_DIR, CONFIG_DIR)
+
     # step 1: get config files
     files = get_config_files_to_list(config_dir)
 
@@ -175,26 +134,36 @@ def process_script():
     file = promt_user_select_file(files)
 
     # step 3: read config file
-    [login_info, description, table, sheetname, properties] = read_config_file(os.path.join(config_dir, file))
+    [login_info, req_tables] = read_config_json(os.path.join(config_dir, file))
 
     # step 4: login apic
     token = get_apic_token(login_info['ip'], login_info['username'], login_info['password'])
 
-    # step 5: get apic data and export to excel
-    outfile = 'result_' + get_datetime() + '.xlsx'
+    # Prepare excel writer
+    outfile = f"apic_{login_info['environment']}_{get_datetime()}.xlsx"
     writer = pd.ExcelWriter(os.path.join(PARENT_DIR, outfile))
-    for i in range(len(table)):
-        # Get apic restapi resp
-        resp = get_apic_api_resp(login_info['ip'], table[i], token)
-        df1 = parse_apic_json_to_df(table[i], resp.json())
-        export_df_to_xlsx(writer, df1, table[i])
+    logger.info(f'###### Step5 - Start processing, export to {outfile}')
+
+    for i in range(len(req_tables)):
+        logger.info(f" ### [{i + 1}/{len(req_tables)}], process {req_tables[i]['key']}")
+        # step 5A: Get apic api
+        resp = get_apic_api_resp(login_info['ip'], req_tables[i]['key'], token)
+        # step 5B: Export to df
+        df1 = parse_apic_json_to_df(req_tables[i]['key'], resp.json())
+        # step 5C: remove properties column
+        if login_info['remove_properties_flag'] == 1:
+            df1 = remove_columns(df1, req_tables[i]['remove_properties'])
+        # step 5D: export to excel
+        export_df_to_xlsx(writer, df1, req_tables[i]['key'])
     writer.close()
+    return
 
 if __name__ == "__main__":
 
     setup_logging()
     logger = logging.getLogger(__name__)
-
+    logger.info(f'###')
+    logger.info(f'###')
     logger.info(f'############################################################## ')
     logger.info(f'##################       START SCRIPT       ################## ')
 
